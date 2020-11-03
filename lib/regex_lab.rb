ee06c6ac1b03e35fcab8ee930ce4126958f1b2b6
Erin Anderson <e.anderson1226@gmail.com>
2020-11-03 16:11:44 -0500

    commit tree 79a0c3218bc39246204d406946cc4155572c1aec

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,17 @@
 def starts_with_a_vowel?(word)
-
+    if(word.scan(/aeiou/))
+        true
+    elsif
+        false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\w+un/)
 end
 
 def words_five_letters_long(text)
-
+    text.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
